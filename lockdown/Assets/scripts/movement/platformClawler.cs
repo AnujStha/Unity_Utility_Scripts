b4793ac2b326a0b4ae0d335ff66eb95f44379c17
AnujStha <40173983+AnujStha@users.noreply.github.com>
2020-05-06 21:39:13 +0545

    player follow movement and platform clawler added

---
+++ lockdown/Assets/scripts/movement/platformClawler.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformClawler : MonoBehaviour
+{
+    [Header("sensors")]
+    public Transform groundCheckPoint, wallcheckPoint;
+    public Vector2 groundCheckBoxCastSize, wallCheckBoxCastSize;
+    public LayerMask platformLayer;
+    [Header("movement")]
+    public float speed,haltRotateAfterRotationTime;
+    private bool haltRotate;
+    private float groundCheckDirection=0, wallCheckDirection=0;
+    void Start()
+    {
+        if (Mathf.Abs(transform.rotation.eulerAngles.z) % 180 == 0)
+        {
+            groundCheckDirection = 0;
+            wallCheckDirection = 0;
+        }
+        else if ((Mathf.Abs(transform.rotation.eulerAngles.z) + 90) % 180 == 0)
+        {
+            groundCheckDirection = 90;
+            wallCheckDirection = 90;
+        }
+    }
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector2.right * speed * Time.fixedDeltaTime);
+        if (!GroundedCheck()&&!haltRotate) {
+            transform.Rotate(new Vector3(0, 0, -90));
+            groundCheckDirection -= 90;
+            wallCheckDirection -= 90;
+            haltRotate = true;
+            StartCoroutine(haltRotateCounter());
+        }
+        if (WallCheck() && !haltRotate) {
+            groundCheckDirection += 90;
+            wallCheckDirection += 90;
+            transform.Rotate(new Vector3(0, 0, 90));
+            haltRotate = true;
+            StartCoroutine(haltRotateCounter());
+        }
+    }
+    bool GroundedCheck()
+    {
+        Collider2D hit = Physics2D.OverlapBox(groundCheckPoint.position, groundCheckBoxCastSize,groundCheckDirection, platformLayer);
+        return hit != null;
+    }
+    bool WallCheck()
+    {
+            Collider2D hit = Physics2D.OverlapBox(wallcheckPoint.position, wallCheckBoxCastSize, wallCheckDirection, platformLayer);     
+            return (hit!=null);
+    }
+    IEnumerator haltRotateCounter() {
+        yield return new WaitForSeconds(haltRotateAfterRotationTime);
+        haltRotate = false;
+    }
+    private void OnDrawGizmosSelected()
+    {
+
+    }
+}
--- lockdown/Assets/scripts/movement/playerFollowMovement.cs
+++ lockdown/Assets/scripts/movement/playerFollowMovement.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class playerFollowMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject player;
+    private Vector2 currentVelocity;
+    public float playerFollowRange,playerMinDistance,acceleration,maxSpeed;
+    [Header("randomMovementBias")]
+    public bool doRandomMovementBias;
+    private bool biasTrigger=true;
+    private Vector2 randomMovementBiasVelocity;
+    public float randomMovementBiasStrength, randomMovementBiasPeriod;
     void Start()
     {
-        
+        player = gameData.player;
     }
-
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float distamce = Vector2.Distance(player.transform.position, transform.position);
+        if (distamce< playerFollowRange&&distamce>playerMinDistance)
+        {
+            Vector2 direction = player.transform.position-transform.position  ;
+            Vector2 force = direction * acceleration * Time.fixedDeltaTime;
+            currentVelocity += force;
+            //speed constraint
+            float speedSquared = currentVelocity.sqrMagnitude;
+            if (speedSquared > maxSpeed * maxSpeed)
+            {
+                currentVelocity = direction.normalized * maxSpeed;
+            }
+        }
+        else {
+            currentVelocity -= currentVelocity * Time.fixedDeltaTime * acceleration;
+        }
+        if (doRandomMovementBias )
+        {
+            if (biasTrigger)
+            {
+                biasTrigger = false;
+                StartCoroutine(biasTigger());
+                randomMovementBiasVelocity = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * randomMovementBiasStrength;
+            }
+            currentVelocity += randomMovementBiasVelocity * Time.fixedDeltaTime;
+        }
+
+        GetComponent<Rigidbody2D>().velocity = currentVelocity;
+    }
+    IEnumerator biasTigger() {
+        yield return new WaitForSeconds(randomMovementBiasPeriod);
+        biasTrigger = true;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, playerFollowRange);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, playerMinDistance);
     }
 }
